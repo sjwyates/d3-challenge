9774a7f25fbb7e04bed6747e8f90e1760ba7628c
Stuart Yates <sjwyates@Mac-mini.local>
2021-01-18 21:41:15 -0700

    vueifying everything

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,68 +1,24 @@
-const svgWidth = 960;
-const svgHeight = 500;
-
-const margin = {
-    top: 20,
-    right: 40,
-    bottom: 80,
-    left: 100
-};
-
-const width = svgWidth - margin.left - margin.right;
-const height = svgHeight - margin.top - margin.bottom;
-
-const svg = d3
-    .select("#scatter")
-    .append("svg")
-    .attr("width", svgWidth)
-    .attr("height", svgHeight);
-
-// Append an SVG group
-const chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
 const app = new Vue({
     el: '#app',
     data: {
+        censusData: null,
+        x: 'obesity',
+        y: 'poverty',
         xOptions: [
-            {
-                key: 'poverty',
-                title: 'In Poverty (%)'
-            },
-            {
-                key: 'age',
-                title: 'Age (Median)'
-            },
-            {
-                key: 'income',
-                title: 'Household Income (Median)'
-            }
+            {key: 'poverty', title: 'In Poverty (%)'},
+            {key: 'age', title: 'Age (Median)'},
+            {key: 'income', title: 'Household Income (Median)'}
         ],
         yOptions: [
-            {
-                key: 'obesity',
-                title: 'Obese (%)'
-            },
-            {
-                key: 'smokes',
-                title: 'Smokes (%)'
-            },
-            {
-                key: 'healthcare',
-                title: 'Lacks Healthcare (%)'
-            }
+            {key: 'obesity', title: 'Obese (%)'},
+            {key: 'smokes', title: 'Smokes (%)'},
+            {key: 'healthcare', title: 'Lacks Healthcare (%)'}
         ],
-        selected: {
-            x: 0,
-            y: 0
-        },
-        data: []
+        svg: {width: 960, height: 500, top: 20, right: 40, bottom: 80, left: 100}
     },
     methods: {
-        xScale: function () {
-
-        },
         renderAxes: function () {
+            const bottomAxis = d3.axisBottom(this.x);
 
         },
         renderCircles: function () {
@@ -72,20 +28,38 @@ const app = new Vue({
 
         }
     },
+    computed: {
+        width: function() {
+            return this.svg.width - this.svg.left - this.svg.right
+        },
+        height: function() {
+            return this.svg.height - this.svg.top - this.svg.bottom;
+        },
+        chartGroupTransform: function() {
+            return `translate(${this.svg.left}, ${this.svg.top})`
+        },
+        xScale: function () {
+            return d3.scaleLinear()
+                .domain([d3.min(this.censusData, state[this.x]) * 0.8,
+                    d3.max(this.censusData, state[this.x]) * 1.2
+                ])
+                .range([0, this.width]);
+        }
+    },
     created: async function () {
-        const data = await d3.csv("./assets/data/data.csv")
-        this.data = data.map(theState => {
-                return {
-                    state: theState.state,
-                    abbr: theState.abbr,
-                    poverty: +theState.poverty,
-                    age: +theState.age,
-                    income: +theState.income,
-                    obesity: +theState.obesity,
-                    smokes: +theState.smokes,
-                    healthcare: +theState.healthcare
-                }
-            })
+        const res = await d3.csv("./assets/data/data.csv")
+        this.censusData = res.map(state => {
+            return {
+                state: state.state,
+                abbr: state.abbr,
+                poverty: +state.poverty,
+                age: +state.age,
+                income: +state.income,
+                obesity: +state.obesity,
+                smokes: +state.smokes,
+                healthcare: +state.healthcare
+            }
+        })
     }
 })
 
