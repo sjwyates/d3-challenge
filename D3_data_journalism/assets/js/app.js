f0536249adf4453554fb143d74a7b85ac842df34
Stuart Yates <sjwyates@Mac-mini.local>
2021-01-18 20:27:08 -0700

    added vue

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,35 @@
-// @TODO: YOUR CODE HERE!
+const app = new Vue({
+    el: '#app',
+    data: {
+        xOptions: [
+            {
+                key: 'poverty',
+                title: 'In Poverty (%)'
+            },
+            {
+                key: 'age',
+                title: 'Age (Median)'
+            },
+            {
+                key: 'income',
+                title: 'Household Income (Median)'
+            }
+        ],
+        yOptions: [
+            {
+                key: 'obesity',
+                title: 'Obese (%)'
+            },
+            {
+                key: 'smokes',
+                title: 'Smokes (%)'
+            },
+            {
+                key: 'healthcare',
+                title: 'Lacks Healthcare (%)'
+            }
+        ],
+        selectedX: 0,
+        selectedY: 0,
+    }
+})
