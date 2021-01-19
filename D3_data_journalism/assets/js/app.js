4cc7c88e0f72d6c0f68472e0c1410ad1ae1851fd
Stuart Yates <sjwyates@Mac-mini.local>
2021-01-18 20:42:08 -0700

    method skeletons

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,3 +1,26 @@
+const svgWidth = 960;
+const svgHeight = 500;
+
+const margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+const width = svgWidth - margin.left - margin.right;
+const height = svgHeight - margin.top - margin.bottom;
+
+const svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+// Append an SVG group
+const chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 const app = new Vue({
     el: '#app',
     data: {
@@ -29,7 +52,28 @@ const app = new Vue({
                 title: 'Lacks Healthcare (%)'
             }
         ],
-        selectedX: 0,
-        selectedY: 0,
+        selected: {
+            x: 0,
+            y: 0
+        },
+        data: []
+    },
+    methods: {
+        xScale: function() {
+
+        },
+        renderAxes: function() {
+
+        },
+        renderCircles: function() {
+
+        },
+        updateToolTip: function() {
+
+        }
+    },
+    created: function () {
+        console.log('selected x: ' + this.selected.x)
     }
 })
+
